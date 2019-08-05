157b71b96d9248649c44200c53b6327607a09a44
Gabriel Valdez <lg.valdez1995@gmail.com>
2019-08-05 04:07:10 -0400

    removed after scenario for gui

--- wdio.conf.js
+++ wdio.conf.js
@@ -252,9 +252,9 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that ran
      */
-    after: async function (result, capabilities, specs) {
-        await DeleteObjects("owner","project");
-    },
+    // after: async function (result, capabilities, specs) {
+    //     await DeleteObjects("owner","project");
+    // },
     /**
      * Gets executed right after terminating the webdriver session.
      * @param {Object} config wdio configuration object
